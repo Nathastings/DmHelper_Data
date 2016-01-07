1ed0cea18a7ef17fbc95effce6e8628336f9e072
Nate <nathastings@gmail.com>
2016-01-07 14:32:49 -0800

    Made sure privileges were setup on the tables

---
+++ MigrationScripts/MigrationScript_1-7-2016.sql
@@ -0,0 +1,10 @@
+﻿ALTER TABLE dmhelper_client.world
+  OWNER TO postgres;
+GRANT ALL ON TABLE dmhelper_client.world TO postgres;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE dmhelper_client.world TO "DB_ReadWrite" WITH GRANT OPTION;
+
+
+ALTER TABLE dmhelper_client.image
+  OWNER TO postgres;
+GRANT ALL ON TABLE dmhelper_client.image TO postgres;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE dmhelper_client.image TO "DB_ReadWrite";
